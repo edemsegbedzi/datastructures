7fde37397206ea8bee762ea46d8260a853209d5d
Edem Segbedzi <segbedzi.edem@gmail.com>
2021-05-04 04:19:12 +0000

    Add addLast method (V2) Add dummy tests in main class

--- src/com/learning/LinkedList.java
+++ src/com/learning/LinkedList.java
@@ -3,6 +3,7 @@ package com.learning;
 public class LinkedList<E>  implements ListI {
 
     Node head;
+    Node tail;
     int currentSize = 0;
 
     public void addFirst(E data){
@@ -10,20 +11,38 @@ public class LinkedList<E>  implements ListI {
         node.next = head;
         head = node;
         currentSize++;
+        if(currentSize == 1){
+            tail = node;
+        }
     }
 
+    // Has a Big O of O(n)
+//    public void addLast(E data){
+//        if(head == null){
+//            head = new Node(data);
+//            currentSize++;
+//            return;
+//        }
+//        Node temp = head;
+//        while(temp.next != null){
+//            temp = temp.next;
+//        }
+//        temp.next = new Node(data);
+//        currentSize++;
+//    }
+
+
+    //has a big O of O(1)
     public void addLast(E data){
+        Node node = new Node(data);
         if(head == null){
-            head = new Node(data);
-            currentSize++;
+            head = tail = node;
+            currentSize ++;
             return;
         }
-        Node temp = head;
-        while(temp.next != null){
-            temp = temp.next;
-        }
-        temp.next = new Node(data);
-        currentSize++;
+        tail.next = node;
+        tail = node;
+        currentSize ++;
     }
 
 
@@ -51,5 +70,26 @@ public class LinkedList<E>  implements ListI {
         public void setData(E data) {
             this.data = data;
         }
+
+        @Override
+        public String toString() {
+            return "Node {" +
+                    "next=" + next +
+                    ", data=" + data +
+                    '}';
+        }
     }
+
+    public void printAll(){
+        if(currentSize == 0){
+            System.out.println("Empty List");
+            return;
+        }
+        Node temp = head;
+        while (temp != null){
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
 }
--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -4,5 +4,17 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+
+        LinkedList linkedList = new LinkedList();
+        linkedList.addLast("E");
+        linkedList.addFirst("D");
+        linkedList.addFirst("C");
+        linkedList.addFirst("B");
+        linkedList.addFirst("A");
+        linkedList.addLast("E");
+        linkedList.addLast("F");
+
+
+        linkedList.printAll();
     }
 }
